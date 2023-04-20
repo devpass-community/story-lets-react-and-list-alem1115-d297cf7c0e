104bb893ce57fb6532f1d020aaad6726a00bb2e9
AlessandroMarc <52158784+AlessandroMarc@users.noreply.github.com>
2023-04-20 21:59:47 +0200

    Added reference

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import Container from './components/Container';
+import Container from "./components/Container";
+import products from "/products.json"; 
 
-const Item = ({ item }) => {
-  return (
-    <li className='item' data-testid='product-item'>
-      { /* TODO */ }
-      <img src="thumbnail here"></img>
-      <h4> title here </h4>
-      <p> description here </p>
-      <span> brand here </span>
-      <span> price here </span>
-    </li>
-  )
+function Item({ item }) {
+    return (
+        <li className="item" data-testid="product-item">
+            {products}
+            <img src="thumbnail here"></img>
+            <h4> title here </h4>
+            <p> description here </p>
+            <span> brand here </span>
+            <span> price here </span>
+        </li>
+    );
 }
 
 function App() {
-  return (
-    <Container>
-      <ul className='list' data-testid='product-list'>
-        { /* TODO */ }
-        <Item />
-      </ul>
-    </Container>
-  );
+    return (
+        <Container>
+            <ul className="list" data-testid="product-list">
+                {/* TODO */}
+                <Item />
+            </ul>
+        </Container>
+    );
 }
 
 export default App;
